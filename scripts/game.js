da697d991099689ee60811d00df8e633f0a43c09
Brian Bird <ProfBird@users.noreply.github.com>
2025-04-16 09:28:54 -0700

    removed totalScore again

--- scripts/game.js
+++ scripts/game.js
@@ -1,4 +1,5 @@
 // Written by Brian Bird, 4/9/2024 with the assistance of GitHub Copilot
+// Revised by Brian Bird 4/16/25
 
 // Global constants used in all classes in the Bunco game
 const BUNCO = 21;    // The winning number of points for a round.
@@ -52,8 +53,12 @@ class Game {
             this.#round = 0;  // this means the game is over
             // the round will be reset to 1 in getGameWinner
         }
+        // add round score to the total score
+        for (const player of this.#players) {
+            player.totalScore += player.roundScore;
+        }
 
-        // reset all the player's round scores
+        // reset all the round scores
         for (const player of this.#players) {
             player.roundScore = 0;
         }
@@ -79,7 +84,7 @@ class Game {
             roundsWon: player.roundsWon
         };
 
-        // if the player scored 0 on their roll, their turn is over
+        // if the player scored 0, their turn is over
         if (rollScore === 0) {
             this.#currentPlayerIndex++;
             if (this.#currentPlayerIndex >= this.#players.length) {
@@ -100,14 +105,6 @@ class Game {
         let roundWinners = this.#players.filter(p => p.roundsWon === maxRoundsWon); if (roundWinners.length === 1) {
             winner = roundWinners[0];
         }
-        else {
-            // if there is a tie, the winner is the player with the highest total score
-            for (let i = 0; i < roundWinners.length; i++) {
-                if (roundWinners[i].totalScore > winner.totalScore) {
-                    winner = roundWinners[i];
-                }
-            }
-        }
         return winner;
     }
 
@@ -115,7 +112,6 @@ class Game {
         this.#round = 1;
         this.#currentPlayerIndex = 0;
         for (const player of this.#players) {
-            player.totalScore = 0;
             player.roundScore = 0;
             player.roundsWon = 0;
         }
